467c2df2092761b6c73b568fa1fbdb5869e2248d
BikeMixel <bixelmike@gmail.com>
2019-01-27 15:27:58 -0500

    small stuff

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -2,9 +2,9 @@ import React from 'react'
 import './Title.css'
 
 const Title = () => (
-    <header className = "Jumbotron">
-    <h1>Animal Clicky Game!</h1>
-    <h3>Click on an Animal Card to score a point, but don't click an image more than once! Score 12 in a row to win. </h3>
+    <header className="Jumbotron">
+    <h1 className="animated slower tada infinite">Animal Clicky Game!</h1>
+    <h3>Click on an Animal Card to score a point, but don't click an image more than <p className="special-p animated slower tada infinite">ONCE!</p></h3> <h2 className="special-h2">Score 12 in a row to win. </h2>
     </header>
 )
 
